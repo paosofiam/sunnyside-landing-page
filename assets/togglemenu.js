de04b7994e3acee09eaf7c58024f6bc3a1030258
Paola Sofia <ev_mendoza@outlook.com>
2024-05-20 12:54:19 -0600

    Responsive Behavior and code structure improved.

--- assets/togglemenu.js
+++ assets/togglemenu.js
@@ -1,7 +1,16 @@
 var tmenu=true;
+var ID0 = document.getElementById("headerContact");
+var ID1 = document.getElementById("ID1");
+var ID2 = document.getElementById("ID2");
+var ID3 = document.getElementById("ID3");
+ID0.addEventListener("click", toggleMenu, false);
+ID1.addEventListener("click", toggleMenu, false);
+ID2.addEventListener("click", toggleMenu, false);
+ID3.addEventListener("click", toggleMenu, false);
 
 function toggleMenu(){
     var menu=document.getElementById('headerList');
+    console.log("there!");
     if(tmenu){
         menu.style.display = "block";
         tmenu=false;
